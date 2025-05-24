777a4561e94149c91ac90af2e43d3fd7b54eb639
ralphleyga67625520 <ralphleyga67625520@outlook.com>
2025-05-25 00:36:23 +0530

    fixing-production-errors so far

--- src/components/ui/container-text-flip.tsx
+++ src/components/ui/container-text-flip.tsx
@@ -33,7 +33,7 @@ export function ContainerTextFlip({
     const updateWidthForWord = () => {
         if (textRef.current) {
             // Add some padding to the text width (30px on each side)
-            // @ts-ignore
+            //@ts-expect-error: textRef.current.scrollWidth is not defined in the type
             const textWidth = textRef.current.scrollWidth + 30;
             setWidth(textWidth);
         }
